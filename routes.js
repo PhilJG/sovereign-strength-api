5283001a7d68524d9253bf3c04f97fb299ae6ea6
Phil <phl.j.gray@gmail.com>
2025-01-12 13:36:48 -0800

    added exercises to csv export

--- routes.js
+++ routes.js
@@ -17,13 +17,50 @@ const filePath = `${__dirname}/workout_data.csv`;
 
 router.get("/csv", async (req, res) => {
   const data = await db.query(`
-    SELECT * FROM workouts`);
+    SELECT w.workout_id, w.workout_date, w.workout_notes, 
+           e.exercise_name, e.exercise_weight, e.reps, e.sets, e.rest_between_sets, e.exercise_complete, e.intensity, e.exercise_notes
+    FROM workouts w
+    LEFT JOIN exercises e ON w.workout_id = e.workout_id
+  `);
 
-  const workoutlist = data.rows;
+  const workouts = {};
+  data.rows.forEach((row) => {
+    if (!workouts[row.workout_id]) {
+      workouts[row.workout_id] = {
+        workout_id: row.workout_id,
+        workout_date: row.workout_date,
+        workout_notes: row.workout_notes,
+        exercises: [],
+      };
+    }
+    workouts[row.workout_id].exercises.push({
+      exercise_name: row.exercise_name,
+      exercise_weight: row.exercise_weight,
+      reps: row.reps,
+      sets: row.sets,
+      rest_between_sets: row.rest_between_sets,
+      exercise_complete: row.exercise_complete,
+      intensity: row.intensity,
+      exercise_notes: row.exercise_notes,
+    });
+  });
 
-  console.log(workoutlist);
+  const csvData = Object.values(workouts).map((workout) => {
+    const exerciseColumns = workout.exercises.reduce((acc, exercise, index) => {
+      acc[`exercise_name_${index + 1}`] = exercise.exercise_name;
+      acc[`exercise_weight_${index + 1}`] = exercise.exercise_weight;
+      acc[`reps_${index + 1}`] = exercise.reps;
+      acc[`sets_${index + 1}`] = exercise.sets;
+      acc[`rest_between_sets_${index + 1}`] = exercise.rest_between_sets;
+      acc[`exercise_complete_${index + 1}`] = exercise.exercise_complete;
+      acc[`intensity_${index + 1}`] = exercise.intensity;
+      acc[`exercise_notes_${index + 1}`] = exercise.exercise_notes;
+      return acc;
+    }, {});
+    return { ...workout, ...exerciseColumns };
+  });
 
-  const header = workoutlist.length > 0 ? Object.keys(workoutlist[0]) : [];
+  const header = Object.keys(csvData[0]);
 
   const csvWriter = createObjectCsvWriter({
     path: "workout_data.csv",
@@ -31,7 +68,7 @@ router.get("/csv", async (req, res) => {
   });
 
   csvWriter
-    .writeRecords(workoutlist)
+    .writeRecords(csvData)
     .then(() => {
       res.download(filePath);
     })
